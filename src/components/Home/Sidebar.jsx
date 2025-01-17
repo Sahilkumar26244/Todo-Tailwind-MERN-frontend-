fe51324fc84c34a2226b0a870d85685239653b1f
Sahil Kumar <kumarsahil8981@gmail.com>
2025-01-17 12:17:54 +0530

    home page side bar completed ui

--- src/components/Home/Sidebar.jsx
+++ src/components/Home/Sidebar.jsx
@@ -1,22 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { CgNotes } from "react-icons/cg";
+import { MdLabelImportantOutline } from "react-icons/md";
+import { FaCheckDouble } from "react-icons/fa6";
+import { TbNotebookOff } from "react-icons/tb";
+
 
 function Sidebar() {
     const data = [
         {
-            title:"All task"
+            title:"All task",
+            icon:<CgNotes/>
         },
         {
-            title:"Important task"
+            title:"Important task",
+            icon:<MdLabelImportantOutline/>
         },
         {
-            title:"Completed task"
+            title:"Completed task",
+            icon:<FaCheckDouble/>
         },
         {
-            title:"Incompleted task"
+            title:"Incompleted task",
+            icon:<TbNotebookOff/>
         }
     ]
   return (
-    <div className='flex flex-col'>
+    <>
         <div>
             <h2 className='text-xl font-semibold'>The Code master</h2>
             <h4 className='mb-1 text-gray-400'>sahil@gmail.com</h4>
@@ -24,12 +33,15 @@ function Sidebar() {
         </div>
         <div>
         {data.map((item,i) => (
-            <div className='my-2 '>
-                {item.title}
+            <div className='my-2 flex items-center gap-3 hover:bg-gray-600 p-2 rounded transition-all duration-100'>
+                {item.icon}{item.title}
             </div>
         ))}
         </div>
-    </div>
+        <div>
+            <button className='bg-gray-600 w-full p-2 rounded'>Log Out</button>
+        </div>
+    </>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Sidebar from '../components/Home/Sidebar'
 function Home() {
   return (
     <div className='flex h-[98vh] gap-4'>
-        <div className='border border-gray-500 rounded-xl w-1/6 p-4'>
+        <div className='border border-gray-500 rounded-xl w-1/6 p-4 flex flex-col justify-between'>
             <Sidebar/>
         </div>
         <div className='border w-5/6 border-gray-500 rounded-xl p-4'>hello 2</div>
