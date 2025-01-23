1a4b95d98f3747a34bbd601690a31ecd513d676e
Sahil Kumar <kumarsahil8981@gmail.com>
2025-01-23 15:05:34 +0530

    login page with functionality completed

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import axios from 'axios';
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 function Login() {
+
+
+  const [data,setData] = useState({username:"",password:""});
+  const history = useNavigate()
+
+  const change = (e) =>{
+    const {name,value} = e.target;
+    setData({...data,[name]:value})
+  }
+
+  console.log(data)
+
+  const submit = async() => {
+    try {
+      if(data.username === "" || data.password === ""){
+        alert("All fields are required!")
+      }
+      else{
+        const response = await axios.post("http://localhost:1000/user/login",data)
+        console.log(response)
+        setData({username:"",password:""})
+        alert(response.data.message)
+        history("/")
+      }
+    } catch (error) {
+      console.log(error.response.data)
+      alert(error.response.data.message)
+
+    }
+  }
+
+
   return (
     <div className=" h-[98vh] flex items-center justify-center">
       <div className="p-4 w-2/6 rounded bg-gray-800">
@@ -11,6 +44,8 @@ function Login() {
           type="username"
           placeholder="Username"
           className="bg-gray-700 px-3 py-2 my-3 w-full rounded"
+          onChange={change}
+          value={data.username}
         />
 
 
@@ -20,9 +55,11 @@ function Login() {
           placeholder="Password"
           className="bg-gray-700 px-3 py-2 my-3 w-full rounded"
           required
+          onChange={change}
+          value={data.password}
         />
         <div className="w-full flex justify-between items-center">
-            <button className="bg-blue-400 text-xl font-semibold text-black px-3 py-2 rounded">Login</button>
+            <button className="bg-blue-400 text-xl font-semibold text-black px-3 py-2 rounded" onClick={submit}>Login</button>
             <Link to="/signup" className='text-gray-700 hover:text-gray-300 '>Not having an account? SignUp here</Link>
         </div>
       </div>
