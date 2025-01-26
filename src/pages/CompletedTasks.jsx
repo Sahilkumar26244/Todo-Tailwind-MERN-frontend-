bfc3362298313b8b3f31b5bd0886c76a3d95dc1e
Sahil Kumar <kumarsahil8981@gmail.com>
2025-01-26 20:52:42 +0530

    completed task is show in frontend

--- src/pages/CompletedTasks.jsx
+++ src/pages/CompletedTasks.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Cards from '../components/Home/Cards'
+import axios from 'axios';
 
 function CompletedTasks() {
+
+
+  const [data,setData] = useState()
+
+  const headers = {
+    id: localStorage.getItem("id"),
+    authorization: `Bearer ${localStorage.getItem("token")}`,
+  };
+
+  const fetchData = async() => {
+    const res = await axios.get("http://localhost:1000/task/getAllCompTasks", {
+      headers,
+    });
+    // console.log(res);
+    setData(res.data.data)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  console.log(data)
+
+
   return (
     <div>
-        <Cards home={"false"} />
+        <Cards home={"false"} data={data} fetchData={fetchData} />
     </div>
   )
 }
--- src/pages/IncompletedTasks.jsx
+++ src/pages/IncompletedTasks.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Cards from '../components/Home/Cards'
+import axios from 'axios';
 
 function IncompletedTasks() {
+
+  const [data,setData] = useState()
+
+  const headers = {
+    id: localStorage.getItem("id"),
+    authorization: `Bearer ${localStorage.getItem("token")}`,
+  };
+
+  const fetchData = async() => {
+    const res = await axios.get("http://localhost:1000/task/getAllCompTasks", {
+      headers,
+    });
+    // console.log(res);
+    setData(res.data.data)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  console.log(data)
   return (
     <div>
         <Cards home={"false"} />
